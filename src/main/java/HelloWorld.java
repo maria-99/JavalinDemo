91b32d8330740a9acac7af7c228c3bf11572d4ea
maria-99 <mariaiman99@gmail.com>
2022-05-14 16:30:21 +0300

    init commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World from Javalin"));
+    }
+}
