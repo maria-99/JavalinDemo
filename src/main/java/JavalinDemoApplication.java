af02f550ea936f136cdf8458acb4eb403a0d97e1
Maria Al Samad <malsamad@dev.vtb.ru>
2022-11-28 11:31:45 +0300

    init

--- src/main/java/JavalinDemoApplication.java
+++ src/main/java/JavalinDemoApplication.java
@@ -1,8 +1,8 @@
 import io.javalin.Javalin;
 
-public class HelloWorld {
+public class JavalinDemoApplication {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7070);
-        app.get("/", ctx -> ctx.result("Hello World from Javalin"));
+        app.get("/hello", ctx -> ctx.result("Hello World from Javalin"));
     }
 }
